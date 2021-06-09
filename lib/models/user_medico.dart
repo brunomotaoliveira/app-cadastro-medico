1f25219dc503e182372acdfc82250a73132f52b6
brunomotadev <brunomotadev@gmail.com>
2021-06-09 18:12:54 -0300

    criado o models/user com os dados do médico

---
+++ lib/models/user_medico.dart
@@ -0,0 +1,16 @@
+class Medico {
+  final String id;
+  final String crm;
+  final String nomeDoMedico;
+  final String endereco;
+  final String telefone;
+  final String valorDaConsulta;
+
+  const Medico(
+      {required this.id,
+      required this.crm,
+      required this.nomeDoMedico,
+      required this.endereco,
+      required this.telefone,
+      required this.valorDaConsulta});
+}
