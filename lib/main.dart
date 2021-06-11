0b9ad4d491a41baade7c09e55504092af7235279
brunomotadev <brunomotadev@gmail.com>
2021-06-11 15:26:09 -0300

    adicionado o provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:app_cadastro_medico/provider/medicos.dart';
 import 'package:app_cadastro_medico/views/user_list_medico.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,12 +11,15 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'CadMedicos',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return ChangeNotifierProvider(
+      create: (context) => Medicos(),
+          child: MaterialApp(
+        title: 'CadMedicos',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: ListaMedico(),
       ),
-      home: ListaMedico(),
     );
   }
 }
---
+++ lib/provider/medicos.dart
@@ -0,0 +1,16 @@
+import 'package:app_cadastro_medico/data/dummy_users.dart';
+import 'package:app_cadastro_medico/models/user_medico.dart';
+import 'package:flutter/material.dart';
+
+class Medicos with ChangeNotifier {
+  final Map<String, Medico> _items = {...DUMMY_USERS};
+
+  List<Medico> get all {
+    return [..._items.values];
+  }
+
+  //retorna quantos elementos tem dentro da lista
+  int get count {
+    return _items.length;
+  }
+}
